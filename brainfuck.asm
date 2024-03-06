362c63f309780185597b75e7f6bdde837f558ef8
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2024-03-06 20:11:28 +0200

    Enable transformation to `.com` format
    - removed procedures
    - fixed stdin redirection

--- brainfuck.asm
+++ brainfuck.asm
@@ -7,86 +7,72 @@
     input    db 10000 dup(0)
 
 .code
-                               org  100h
-
-readArgument proc
-                               mov  si, offset 82h                ; Set SI to point to the command line
-    copyLoop:                  
-                               mov  al, [si]                      ; Load character from command line
-                               cmp  al, 0Dh                       ; Compare with carriage return
-                               je   finishCopy
-                               mov  [filename+di], al
-                               inc  di
-                               inc  si                            ; Move to next character in command line
-                               jmp  copyLoop
-
-    finishCopy:                
-                               mov  byte ptr [filename+di], 0     ; Null-terminate filename
-                               ret
-readArgument endp
-
-getFileContentIntoVariable proc
-    ; Open file
-                               mov  ah, 3Dh
-                               mov  al, 0                         ; Open file for reading
-                               lea  dx, filename
-                               int  21h
-                               mov  bx, ax                        ; Save file handle
-
-    ; Read file content into the code variable
-                               mov  ah, 3Fh
-                               lea  dx, code
-                               mov  cx, 10000                     ; Number of bytes to read
-                               int  21h
-
-    ; Save number of bytes read
-                               mov  codeLen, ax
-
-    ; Close file
-                               mov  ah, 3Eh
-                               xor  bx, bx
-                               int  21h
-                               ret
-getFileContentIntoVariable endp
-
-readInput proc
-                               xor  bx, bx                        ; Clear input index
-    readChar:                  
-                               mov  ah, 01h                       ; Function code for reading char from stdin
-                               int  21h
-                               cmp  al, 0Dh                       ; Compare with carriage return
-                               je   finishRead                    ; If Enter, finish reading
-                               mov  [input+bx], al
-                               inc  bx
-                               jmp  readChar
-    finishRead:                
-                               mov  [input+bx], 0                 ; Null-terminate the string
-                               ret
-readInput endp
-
-writeOutput proc
-                               xor  bx, bx                        ; Clear input index
-    writeChar:                 
-                               cmp  [input+bx], 0                 ; Check for null-terminator
-                               je   finishOutput
-                               mov  dl, [input+bx]
-                               mov  ah, 02h                       ; Function code for writing char to stdout
-                               int  21h
-                               inc  bx
-                               jmp  writeChar
-    finishOutput:              
-                               ret
-writeOutput endp
-                               
+                org 100h
 
 main proc
-                               call readArgument
-                               call readInput
-                               call getFileContentIntoVariable
-                               call writeOutput
+    ; Read argument
+                xor di, di                       ; Clear filename index
+                mov si, offset 82h               ; Set SI to point to the command line
+    copyLoop:   
+                mov al, [si]                     ; Load character from command line
+                cmp al, 0Dh                      ; Compare with carriage return
+                je  finishCopy
+                mov [filename+di], al
+                inc di
+                inc si                           ; Move to next character in command line
+                jmp copyLoop
 
-                               mov  ah, 4ch
-                               int  21h
+    finishCopy: 
+                mov byte ptr [filename+di], 0    ; Null-terminate filename
+
+    ; Read input
+                xor bx, bx                       ; Clear input index
+    readChar:   
+                mov ah, 3Fh                      ; Stdin function code
+                mov bx, 0                        ; File handle 0 (stdin)
+                lea dx, input                    ; Pointer to the input buffer
+                mov cx, 10000                    ; Maximum number of bytes to read
+                int 21h
+                mov di, ax                       ; Save number of bytes read
+                mov [input+di], 0                ; Null-terminate input
+
+    ; Write output
+                xor bx, bx                       ; Clear input index
+    writeChar:  
+                cmp [input+bx], 0                ; Check for null-terminator
+                je  finishWrite
+                mov dl, [input+bx]
+                mov ah, 02h                      ; Function code for writing char to stdout
+                int 21h
+                inc bx
+                jmp writeChar
+    finishWrite:
+                xor bx, bx                       ; Clear input index
+    
+    ; Get content of code file into code variable
+    ; Open file
+                mov ah, 3Dh
+                mov al, 0                        ; Open file for reading
+                lea dx, filename
+                int 21h
+                mov bx, ax                       ; Save file handle
+
+    ; Read file content into the code variable
+                mov ah, 3Fh
+                lea dx, code
+                mov cx, 10000                    ; Number of bytes to read
+                int 21h
+
+    ; Save number of bytes read
+                mov codeLen, ax
+
+    ; Close file
+                mov ah, 3Eh
+                int 21h
+                 
+    finish:     
+                mov ah, 4ch
+                int 21h
 
 main endp
 end main
