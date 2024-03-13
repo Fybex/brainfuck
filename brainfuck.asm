0ec5cf5942645569a745afff3ae326a8d845ffda
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2024-03-13 21:49:53 +0200

    Fix stdin/stdout (first version that passes tests)
    - transform 0Dh 0Ah into 0Ah;
    - read next if 0Dh;
    - set -1 if EOF
    - add 0Dh before 0Ah on sout

--- brainfuck.asm
+++ brainfuck.asm
@@ -9,40 +9,41 @@
                       org  100h
 
 main proc
-                      xor  di, di                       ; Tape pointer
-                      mov  cx, 10000                    ; Tape length
+    ; Clean tape
+                      xor  di, di                          ; Tape pointer
     clearTape:        
-                      mov  byte ptr [tape + di], 0
+                      mov  word ptr [tape + di], 0
                       add  di, 2
-                      loop clearTape                    ; Loop until CX is 0
+                      cmp  di, 20000
+                      jne  clearTape
 
     ; Read argument
-                      xor  di, di                       ; Clear filename index
-                      mov  si, offset 82h               ; Set SI to point to the command line
+                      xor  di, di                          ; Clear filename index
+                      mov  si, offset 82h                  ; Set SI to point to the command line
     copyLoop:         
-                      mov  al, [si]                     ; Load character from command line
-                      cmp  al, 0Dh                      ; Compare with carriage return
+                      mov  al, [si]                        ; Load character from command line
+                      cmp  al, 0Dh                         ; Compare with carriage return
                       je   finishCopy
                       mov  [filename+di], al
                       inc  di
-                      inc  si                           ; Move to next character in command line
+                      inc  si                              ; Move to next character in command line
                       jmp  copyLoop
 
     finishCopy:       
-                      mov  byte ptr [filename+di], 0    ; Null-terminate filename
+                      mov  byte ptr [filename+di], 0       ; Null-terminate filename
     
     ; Get content of code file into code variable
     ; Open file
                       mov  ah, 3Dh
-                      mov  al, 0                        ; Open file for reading
+                      mov  al, 0                           ; Open file for reading
                       lea  dx, filename
                       int  21h
-                      mov  bx, ax                       ; Save file handle
+                      mov  bx, ax                          ; Save file handle
 
     ; Read file content into the code variable
                       mov  ah, 3Fh
                       lea  dx, code
-                      mov  cx, 10000                    ; Number of bytes to read
+                      mov  cx, 10000                       ; Number of bytes to read
                       int  21h
 
     ; Save number of bytes read
@@ -53,15 +54,15 @@ main proc
                       int  21h
 
     ; Intepreter
-                      xor  di, di                       ; Tape pointer
-                      xor  si, si                       ; Code pointer
+                      xor  di, di                          ; Tape pointer
+                      xor  si, si                          ; Code pointer
 
     interpretLoop:    
-                      cmp  si, codeLen                  ; Check if end of code
+                      cmp  si, codeLen                     ; Check if end of code
                       jne  interpretContinue
                       jmp  finish
     interpretContinue:
-                      mov  al, [code + si]              ; Load the current command
+                      mov  al, [code + si]                 ; Load the current command
 
     ; Command switch
                       cmp  al, '+'
@@ -81,7 +82,7 @@ main proc
                       cmp  al, ','
                       je   inputChar
 
-                      inc  si                           ; Skip unknown commands
+                      inc  si                              ; Skip unknown commands
                       jmp  interpretLoop
 
     ; Commands
@@ -102,21 +103,21 @@ main proc
                       jmp  nextCommand
 
     startLoop:        
-                      push si                           ; Save loop start pointer
+                      push si                              ; Save loop start pointer
                       cmp  word ptr [tape + di], 0
-                      jz   findLoopEnd                  ; Skip loop if 0
+                      jz   findLoopEnd                     ; Skip loop if 0
                       jmp  nextCommand
 
     endLoop:          
                       cmp  word ptr [tape + di], 0
-                      jnz  repeatLoop                   ; Jump back to start if not 0
-                      add  sp, 2                        ; Clean up the stack
+                      jnz  repeatLoop                      ; Jump back to start if not 0
+                      add  sp, 2                           ; Clean up the stack
                       jmp  nextCommand
 
     findLoopEnd:      
-                      mov  cx, 1                        ; Increase loop nest level
+                      mov  cx, 1                           ; Increase loop nest level
     searchLoopEnd:    
-                      inc  si                           ; Next command
+                      inc  si                              ; Next command
                       cmp  byte ptr [code + si], '['
                       je   increaseLoopNest
                       cmp  byte ptr [code + si], ']'
@@ -128,30 +129,40 @@ main proc
     decreaseLoopNest: 
                       dec  cx
                       jnz  searchLoopEnd
-                      add  sp, 2                        ; Clean up the stack
+                      add  sp, 2                           ; Clean up the stack
                       jmp  nextCommand
 
     repeatLoop:       
-                      pop  si                           ; Get loop start address
-                      push si                           ; Save it again
+                      pop  si                              ; Get loop start address
+                      push si                              ; Save it again
                       jmp  nextCommand
 
     output:           
                       mov  dx, [tape + di]
-                      mov  ah, 02h                      ; Stdout function code
+                      mov  ah, 02h                         ; Stdout function code
+                      cmp  word ptr [tape + di], 0Ah       ; Check if it's a newline
+                      jne  outputContinue
+                      mov  dl, 0Dh                         ; Add carriage return before newline
+                      int  21h
+    outputContinue:   
+                      mov  dx, [tape + di]
                       int  21h
                       jmp  nextCommand
 
     inputChar:        
-                      mov  ah, 3Fh                      ; Stdin function code
+                      mov  ah, 3Fh                         ; Stdin function code
                       mov  bx, 0
-                      lea  dx, tape
-                      add  dx, di                       ; Offset into the tape
-                      push cx
-                      mov  cx, 1                        ; Number of bytes to read
+                      lea  dx, [tape + di]                 ; Offset into the tape
+                      push cx                              ; Save loop counter
+                      mov  cx, 1                           ; Number of bytes to read
                       int  21h
-                      pop  cx
-                      jmp  nextCommand
+                      pop  cx                              ; Restore loop counter
+                      or   ax, ax
+                      jnz  skipEOF
+                      mov  word ptr [tape + di], 0FFFFh    ; Set to -1 if EOF
+    skipEOF:          
+                      cmp  word ptr [tape + di], 0Dh       ; Read again if it's a carriage return
+                      je   inputChar
                     
     nextCommand:      
                       inc  si
