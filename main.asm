c94c67b6012cad53667ec127509a9b55134be7ad
Andrii Valenia <46112160+Fybex@users.noreply.github.com>
2024-03-26 00:37:51 +0200

    Autoformat file

--- main.asm
+++ main.asm
@@ -8,10 +8,10 @@
 
 main proc
     ; Clean tape and code
-                      mov   di, offset tape        ; Tape pointer
-                      push  di                     ; Save tape pointer
-                      mov   cx, 20000              ; Number of cells
-                      push  cx                     ; Save 20000
+                      mov   di, offset tape       ; Tape pointer
+                      push  di                    ; Save tape pointer
+                      mov   cx, 20000             ; Number of cells
+                      push  cx                    ; Save 20000
                       rep   stosw
 
     ; Read argument
@@ -20,17 +20,17 @@ main proc
                       
 
     ; Open file using command line argument directly
-                      mov   ah, 3Dh                ; Open file for reading
+                      mov   ah, 3Dh               ; Open file for reading
     ; DX = Pointer to the file name
-                      mov  dx, 82h                 ; Pointer to the command line argument
+                      mov   dx, 82h               ; Pointer to the command line argument
                       int   21h
-                      xchg  bx, ax                 ; Save file handle
+                      xchg  bx, ax                ; Save file handle
 
     ; Read file content into the code variable
                       mov   ah, 3Fh
-                      pop   cx                     ; Number of bytes to read (need only 10000, but 20000 is ok as well)
+                      pop   cx                    ; Number of bytes to read (need only 10000, but 20000 is ok as well)
                       mov   dx, offset code
-                      push  dx                     ; Save the pointer to the code variable
+                      push  dx                    ; Save the pointer to the code variable
                       int   21h
 
     ; Close file
@@ -38,15 +38,15 @@ main proc
                       int   21h
 
     ; Intepreter
-                      xor   bx, bx                 ; Stdin file handle (bx = 0)
+                      xor   bx, bx                ; Stdin file handle (bx = 0)
                         
-                      pop   si                     ; Restore code pointer
-                      pop   di                     ; Restore tape pointer
+                      pop   si                    ; Restore code pointer
+                      pop   di                    ; Restore tape pointer
                       
 
     interpretLoop:    
-                      lodsb                        ; Load the current command
-                      test  al, al                 ; Zero if code ends
+                      lodsb                       ; Load the current command
+                      test  al, al                ; Zero if code ends
                       jnz   increment
     ; End of program
                       ret
@@ -80,27 +80,27 @@ main proc
                       cmp   al, '['
                       jne   endLoop
                       cmp   word ptr [di], bx
-                      jz    findLoopEnd            ; Skip loop if 0
-                      push  si                     ; Save loop start pointer
+                      jz    findLoopEnd           ; Skip loop if 0
+                      push  si                    ; Save loop start pointer
 
     endLoop:          
                       cmp   al, ']'
                       jne   output
                       cmp   word ptr [di], bx
-                      jnz   repeatLoop             ; Jump back to start if not 0
-                      pop   dx                     ; Clean up the stack in non-used dx register
+                      jnz   repeatLoop            ; Jump back to start if not 0
+                      pop   dx                    ; Clean up the stack in non-used dx register
                       jmp   interpretLoop
     repeatLoop:       
-                      pop   si                     ; Get loop start address
-                      push  si                     ; Save it again
+                      pop   si                    ; Get loop start address
+                      push  si                    ; Save it again
 
     output:           
                       cmp   al, '.'
                       jne   inputChar
-                      mov   ah, 02h                ; Stdout function code
-                      cmp   word ptr [di], 0Ah     ; Check if it's a newline
+                      mov   ah, 02h               ; Stdout function code
+                      cmp   word ptr [di], 0Ah    ; Check if it's a newline
                       jne   outputContinue
-                      mov   dl, 0Dh                ; Add carriage return before newline
+                      mov   dl, 0Dh               ; Add carriage return before newline
                       int   21h
     outputContinue:   
                       mov   dx, [di]
@@ -110,31 +110,31 @@ main proc
                       cmp   al, ','
                       jne   interpretLoop
     inputCharContinue:
-                      mov   ah, 3Fh                ; Stdin function code
-                      mov   word ptr [di], bx      ; Clear the current cell to hold input correctly
-                      lea   dx, [di]               ; Offset into the tape
-                      push  cx                     ; Save loop counter
-                      mov   cx, 1                  ; Number of bytes to read
+                      mov   ah, 3Fh               ; Stdin function code
+                      mov   word ptr [di], bx     ; Clear the current cell to hold input correctly
+                      lea   dx, [di]              ; Offset into the tape
+                      push  cx                    ; Save loop counter
+                      mov   cx, 1                 ; Number of bytes to read
                       int   21h
-                      pop   cx                     ; Restore loop counter
-                      test  ax, ax                 ; If 0 bytes read, it's EOF
+                      pop   cx                    ; Restore loop counter
+                      test  ax, ax                ; If 0 bytes read, it's EOF
                       jnz   skipEOF
-                      dec   word ptr [di]          ; Set to -1 if EOF (it was 0 before, so dec can be used)
+                      dec   word ptr [di]         ; Set to -1 if EOF (it was 0 before, so dec can be used)
     skipEOF:          
-                      cmp   word ptr [di], 0Dh     ; Read again if it's a carriage return
+                      cmp   word ptr [di], 0Dh    ; Read again if it's a carriage return
                       je    inputCharContinue
                       jmp   interpretLoop
 
     findLoopEnd:      
-                      mov   cx, 1                  ; Increase loop nest level
+                      mov   cx, 1                 ; Increase loop nest level
     searchLoopEnd:    
-                      lodsb                        ; Next command
+                      lodsb                       ; Next command
                       cmp   al, '['
                       je    increaseLoopNest
                       cmp   al, ']'
                       jne   searchLoopEnd
     ; decreaseLoopNest
-                      loop  searchLoopEnd          ; Loop until cx = 0
+                      loop  searchLoopEnd         ; Loop until cx = 0
                       jmp   interpretLoop
     increaseLoopNest: 
                       inc   cx
